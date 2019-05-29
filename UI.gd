894539f23d8935dfc94cc0cc759cb5fece26453e
Erik Fercak <erik.fercak@fluxfederation.com>
2019-05-29 17:58:39 +1200

    Add basic UI elements

---
+++ UI.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	lives_changed(1)
+	score_changed(0)
+	$Lives/AnimationPlayer.play("Blinking");
+	
+
+func lives_changed(lives):
+	$Lives.text = "%sUP" % [lives]
+	
+func score_changed(score):
+	$Score.text = "Score: %s" % [score]
